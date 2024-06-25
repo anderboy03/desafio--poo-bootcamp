9471dcc654fd73bfe7cb720e833a3b6c18a6c9d4
Anderfoxx <anderfox1988@gmail.com>
2024-06-25 11:11:06 -0300

    commit mntoria

---
+++ POO/src/Main.java
@@ -0,0 +1,22 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 =new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 =new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(8);
+
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        
+    }
+
+}
